12d12f4dc79e9baeec1e8e0c51a2572bbad5a054
= <=>
2017-12-04 16:23:13 +0000

    add potential code

---
+++ potentialFB.m
@@ -0,0 +1,16 @@
+V_bi = linspace(0,500);
+Nd = 1;
+q = 1;
+e = 0.977;
+x = linspace(0,15);
+V = abs(V_bi);
+D = sqrt(((2*e)/(q*Nd)) * V);
+U = ((q.^2 * Nd) / (2*e)) * (D-x).^2;
+% for n = 2:(length(U)-1)
+%     if(U(n)>U(n-1))
+%        U = U(1:n);
+%        break
+%     end
+% end
+% x = x(1:length(U));
+plot(x,U);
